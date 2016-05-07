50e270de899c2ce60e08a30d6392a4caf5c064a2
Fran Álvarez <fjalvarez@gmail.com>
2016-05-07 17:01:03 +0200

    Mock

--- app/src/App/Services/CitiesService.php
+++ app/src/App/Services/CitiesService.php
@@ -7,6 +7,15 @@ class CitiesService extends BaseService
 
     public function getAll()
     {
-        return array('test');
+        $mallorca = array(
+            'name' => 'Palma de Mallorca',
+            'picture' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
+            'city_id' => 1,
+            'booking_id' => 1,
+            'hotelbeds_id' => 2,
+            'musements_id' => 3,
+        );
+        $mockedCities[] = $mallorca;
+        return $mockedCities;
     }
 }
