635c5a0379848b62c710c92f729efc752caad8bb
Fran Álvarez <fjalvarez@gmail.com>
2016-05-07 23:47:16 +0200

    Include HB

--- app/src/App/Services/HotelsService.php
+++ app/src/App/Services/HotelsService.php
@@ -2,9 +2,65 @@
 
 namespace App\Services;
 
+use DateTime;
+use hotelbeds\hotel_api_sdk\HotelApiClient;
+use hotelbeds\hotel_api_sdk\model\Destination;
+use hotelbeds\hotel_api_sdk\model\Occupancy;
+use hotelbeds\hotel_api_sdk\model\Pax;
+use hotelbeds\hotel_api_sdk\model\Stay;
+use hotelbeds\hotel_api_sdk\types\ApiVersion;
+use hotelbeds\hotel_api_sdk\types\ApiVersions;
+
 class HotelsService extends BaseService
 {
     public function getByCoordinates($checkIn, $checkOut, $latitude, $longitude)
+    {
+        return array(
+            'hotelbeds' => $this->getHotelBedsByCoordinates($checkIn, $checkOut, $latitude, $longitude),
+            'booking' => $this->getBookingByCoordinates($checkIn, $checkOut, $latitude, $longitude),
+        );
+    }
+
+    protected function getHotelBedsByCoordinates($checkIn, $checkOut, $latitude, $longitude)
+    {
+
+        $cfgApi = array(
+            'url' => 'https://api.test.hotelbeds.com',
+            'apiKey' => "t3gv6hdbhs23xtsem4p2auan",
+            'sharedSecret' => 'tFjZCmhCVh',
+            'timeout' => 120
+        );
+
+        $apiClient = new HotelApiClient($cfgApi["url"],
+            $cfgApi["apiKey"],
+            $cfgApi["sharedSecret"],
+            new ApiVersion(ApiVersions::V1_0),
+            $cfgApi["timeout"]);
+
+        $rqData = new \hotelbeds\hotel_api_sdk\helpers\Availability();
+        $rqData->stay = new Stay(DateTime::createFromFormat("Y-m-d", "2016-07-01"),
+            DateTime::createFromFormat("Y-m-d", "2016-07-10"));
+
+        $rqData->destination = new Destination("PMI");
+        $occupancy = new Occupancy();
+        $occupancy->adults = 2;
+        $occupancy->children = 1;
+        $occupancy->rooms = 1;
+
+        $occupancy->paxes = [new Pax(Pax::AD, 30, "Mike", "Doe"), new Pax(Pax::AD, 27, "Jane", "Doe"), new Pax(Pax::CH, 8, "Mack", "Doe")];
+        $rqData->occupancies = [$occupancy];
+
+        $availRS = $apiClient->Availability($rqData);
+        $result = $availRS->hotels->toArray();
+        print_r($result);
+        die;
+        die;
+
+//        return json_decode($response->getBody());
+
+    }
+
+    protected function getBookingByCoordinates($checkIn, $checkOut, $latitude, $longitude)
     {
         $url = "https://hacker232:fthriQ0ZWfs@distribution-xml.booking.com/json/getHotelAvailabilityV2?latitude=" .
             $latitude . "&longitude=" . $longitude . "&checkin={$checkIn}&checkout={$checkOut}&room1=A,A&" .
