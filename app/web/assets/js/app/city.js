24fa92f98733fbde4ccabfabfb3a88c85e11b56c
Fran Álvarez <fjalvarez@gmail.com>
2016-05-08 01:27:29 +0200

    Fix markup

--- app/web/assets/js/app/city.js
+++ app/web/assets/js/app/city.js
@@ -1,13 +1,13 @@
 
 
 $(document).ready(function () {
-	$('#event-selector').click(function() {
+	$('.event-selector').click(function() {
 		$(this).toggleClass('event-selected');
 	});
 
 	$('#search-hotels').click(function() {
 		var coordinates = {};
-		$.each($('#event-selector').hasClass('event-selected'), function() {
+		$.each($('.event-selector').hasClass('event-selected'), function() {
 			console.log($(this).data('latitude'));
 		});
 		$('#event-selected').data('latitude')
