7fc5ce65e84b888a62ab67dc8962bd385f87e08e
inakivb <villaburbula@gmail.com>
2016-05-07 18:11:25 +0200

    Añadiendo ids

--- app/src/App/Controllers/HotelsController.php
+++ app/src/App/Controllers/HotelsController.php
@@ -21,4 +21,67 @@ class HotelsController
         $coordinates = $request->request->get("coordinates");
         return new JsonResponse($this->hotelsService->getByCoordinates($coordinates));
     }
+    public function getBestGeoByGeos($geoArray){
+        foreach($geoArray as $k=>$geo){
+            $geoArray[$k][]=0;
+        }
+        return $this->getMedian($geoArray,100);
+    }
+
+    function addArray($arr1, $arr2) {
+        foreach (array_intersect_key($arr2, $arr1) as $key => $val) $arr1[$key] += $val;
+        $arr1 += $arr2;
+        return $arr1;
+    }
+    function subtractArray($arr1, $arr2) {
+        foreach($arr1 as $key => &$val){
+            if(isset($arr2[$key])){
+                $val -= $arr2[$key];
+            }
+        }
+        return $arr1;
+    }
+    function vectorNorm($array){
+        $squareSum = 0;
+        foreach($array as $value){
+            $squareSum += pow($value,2);
+        }
+        $norm = sqrt($squareSum);
+        return $norm;
+    }
+    function multiplyArray($arr, $scalar){
+        return array_map(
+            function($val, $factor) { return $val * $factor; },
+            $arr,
+            array_fill(0, count($arr), $scalar)
+        );
+    }
+    function divideArray($arr, $scalar){
+        return array_map(
+            function($val, $factor) { return $val / $factor; },
+            $arr,
+            array_fill(0, count($arr), $scalar)
+        );
+    }
+    function weiszfeld($dataArray,$previousEstimate){
+        $numerator = [0];
+        $denominator = 0;
+        $keys = array_keys($dataArray[0]);
+        foreach ($dataArray as $dataPoint) {
+            $numerator = $this->addArray($numerator, $this->divideArray($dataPoint,$this->vectorNorm($this->subtractArray($dataPoint,$previousEstimate))));
+        }
+        foreach ($dataArray as $dataPoint) {
+            $denominator = $denominator + 1/($this->vectorNorm($this->subtractArray($dataPoint,$previousEstimate)));
+        }
+        $median = $this->divideArray($numerator,$denominator);
+        $median = array_combine($keys,$median);
+        return $median;
+    }
+    function getMedian($dataArray,$iterations){
+        $median = $this->weiszfeld($dataArray,[0,0,0,0,0,0]);
+        for($i=0;$i<$iterations;$i++){
+            $median = $this->weiszfeld($dataArray,$median);
+        }
+        return $median;
+    }
 }
--- app/src/App/Services/HotelsService.php
+++ app/src/App/Services/HotelsService.php
@@ -6,22 +6,10 @@ class HotelsService extends BaseService
 {
     public function getByCoordinates($coordinates)
     {
-        $url = "https://app.xapix.io/api/v1/thm16_tech_beatles/city_events?filter[city_id]={$id}".
-            "&fields[city_events]=city_id,url,longitude,latitude,reviews_avg,reviews_number,discount,".
-            "net_price_0_formatted_value,net_price_0_value,net_price_0_currency,retail_price_0_formatted_value,".
-            "retail_price_0_value,retail_price_0_currency,cover_image_url,is_available_tomorrow,is_available_today,".
-            "daily,exclusive,ticket_not_included,open,duration,about,description,temporary,top_seller,last_chance,".
-            "must_see,max_confirmation_time,relevance,title,saves,city_0_url,city_0_cover_image_url,city_1_name,".
-            "city_1_x_id,city_0_name,city_0_x_id,x_id&sort=x_id&page[number]=1&page[size]=1000";
+        $url = "https://hacker232:fthriQ0ZWfs@distribution-xml.booking.com/json/getHotelAvailabilityV2?latitude=40.415258152971&longitude=-3.7148935609517&checkin=2016-07-01&checkout=2016-07-02&room1=A,A";
         $client = new \GuzzleHttp\Client();
 
-        $result = $client->request('GET', $url, [
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-                'Authorization-Token' => 'GAes76CZnkHTPJoBV0rtgz5Y9Fiq2LQI',
-            ],
-        ]);
+        $result = $client->request('GET', $url);
 
         if ($result->getStatusCode() == 200) {
             return json_decode($result->getBody(), true);
