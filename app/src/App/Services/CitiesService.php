170816c871d30e2eb2968fa56f17bd606417caa2
inakivb <villaburbula@gmail.com>
2016-05-07 17:21:15 +0200

    Añadiendo ids

--- app/src/App/Services/CitiesService.php
+++ app/src/App/Services/CitiesService.php
@@ -7,15 +7,36 @@ class CitiesService extends BaseService
 
     public function getAll()
     {
-        $mallorca = array(
-            'name' => 'Palma de Mallorca',
-            'picture' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
-            'city_id' => 1,
-            'booking_id' => 1,
-            'hotelbeds_id' => 2,
-            'musements_id' => 3,
+        $mockedCities = array(
+            array(
+                'name' => 'Palma de Mallorca',
+                'picture' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
+                'thumbnail' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
+                'city_id' => 1,
+                'booking_id' => -345224,
+                'hotelbeds_id' => 0,
+                'musements_id' => 216,
+            ),
+            array(
+                'name' => 'Barcelona',
+                'picture' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
+                'thumbnail' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
+                'city_id' => 2,
+                'booking_id' => -372490,
+                'hotelbeds_id' => 0,
+                'musements_id' => 60,
+            ),
+            array(
+                'name' => 'Berlin',
+                'picture' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
+                'thumbnail' => 'http://www.spain.info/export/sites/spaininfo/comun/carrusel-recursos/baleares/vista-palma-de-mallorca-2183112-istock.jpg_369272544.jpg',
+                'city_id' => 3,
+                'booking_id' => -1746443,
+                'hotelbeds_id' => 0,
+                'musements_id' => 52,
+            ),
         );
-        $mockedCities[] = $mallorca;
+
         return $mockedCities;
     }
 }
