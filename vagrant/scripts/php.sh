7f4e6dabdfe04f664ccf394a94cfaba5f13fbc9b
Fran Álvarez <fjalvarez@gmail.com>
2016-05-07 15:08:34 +0200

    Remove xdebug

--- vagrant/scripts/php.sh
+++ vagrant/scripts/php.sh
@@ -2,7 +2,7 @@
 
 echo "Installing PHP..."
 
-sudo apt-get -y install php5 php5-cli php5-dev php5-fpm php5-curl php5-mcrypt php5-memcache php5-memcached php5-xsl php5-tidy php5-xdebug php5-mysqlnd  php5-cli php5-mysqlnd php5-curl php5-gd php5-memcached php5-memcache php5-fpm php5-tidy php5-xdebug php5-dev php-apc libpcre3-dev php5-xmlrpc php5-odbc
+sudo apt-get -y install php5 php5-cli php5-dev php5-fpm php5-curl php5-mcrypt php5-memcache php5-memcached php5-xsl php5-tidy php5-mysqlnd  php5-cli php5-mysqlnd php5-curl php5-gd php5-memcached php5-memcache php5-fpm php5-tidy php5-dev php-apc libpcre3-dev php5-xmlrpc php5-odbc
 
 sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
